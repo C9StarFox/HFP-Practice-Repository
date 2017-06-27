d7f42ca2bf1262c5328a7634539e1d8ef39d0fd9
C9StarFox <aaho1995@gmail.com>
2017-06-27 12:02:33 -0500

    Add files via upload

---
+++ nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'nester',
+        version         = '1.0.0',
+        py_modules      = ['nester'],
+        author          = 'c9starf0x',
+        author_email    = 'c9starf0x@gmail.com',
+        url             = 'http://www.facebook.com',
+        description     = 'A printer of nested lists',
+    )
